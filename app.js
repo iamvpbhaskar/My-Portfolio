4662e6ab2bb9a0706d5cf70ac98c4952fdce1923
Ved Prakash Bhaskar <iamved99@gmail.com>
2022-07-17 20:46:31 +0530

    portfolio done

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ function toggleButton(){
 }
 hamburgerButton.addEventListener('click',toggleButton);
 
+dayNightTheme = () => {
+    let date = new Date();
+    let hour = date.getHours();
+  
+    if(hour >= 7 && hour < 19){
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+    }
+    else{
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+    }
+  }
+  
+  window.addEventListener('load', dayNightTheme);
+  
+
